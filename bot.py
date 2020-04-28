d3567359e7a104e330cedafb60b5f7a4ddbb99f1
Natanael Tan <nonnoxer@gmail.com>
2020-04-28 10:56:19 +0800

    short updates

--- bot.py
+++ bot.py
@@ -17,7 +17,17 @@ async def on_ready():
 
 @bot.command(name="avalon", help="Initiates a game of Avalon")
 async def avalon(ctx):
-    await ctx.send("Start a game of Avalon?")
+    await ctx.send("Start a game of Avalon? [Y]es/[N]o")
+    def check(message):
+        return message.author == ctx.author
+    reply = await bot.wait_for("message", check=check)
+    reply_content = reply.content.lower()
+    if reply_content == "y" or reply_content == "yes":
+        await ctx.send("The quest begins!")
+    elif reply_content == "n" or reply_content == "no":
+        await ctx.send("Another day then.")
+    else:
+        await ctx.send("You speak in riddles.")
 
 @bot.event
 async def on_command_error(ctx, error):
--- cogs.py
+++ cogs.py
@@ -1,6 +1,6 @@
 from discord.ext import commands
 
-class Game(commands.Cog):
+class Avalon(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 

591e1980a898c82ad07d946a46d48eee8d51c66b
Natanael Tan <nonnoxer@gmail.com>
2020-04-28 18:02:15 +0800

    initiate game

--- cogs.py
+++ cogs.py
@@ -1,8 +1,31 @@
 from discord.ext import commands
 
 class Avalon(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot, ctx, user):
         self.bot = bot
+        self.ctx = ctx
+        self.players = [user]
+        self.host = user
+
+    @commands.command()
+    async def join(self, ctx):
+        if ctx.author not in self.players and len(self.players) <= 10:
+            self.players.append(ctx.author)
+            await ctx.send(f"{ctx.author} has joined the game!\nPlayer slots: {len(self.players)} / 10")
+        elif ctx.author in self.players:
+            await ctx.send("You are already in the game.")
+        else:
+            await ctx.send("The game is full.")
+
+    @commands.command()
+    async def start(self, ctx):
+        if ctx.author == self.host:
+            if len(self.players) >= 5:
+                await ctx.send(f"The game begins!\nPlayer count: {len(self.players)} / 10")
+            else:
+                await ctx.send(f"The game is too empty.\nPlayer slots: {len(self.players)} / 10")
+        else:
+            await ctx.send("You are not the host.")
 
 class Round(commands.Cog):
     pass
--- main.py
+++ main.py
@@ -17,17 +17,16 @@ async def on_ready():
 
 @bot.command(name="avalon", help="Initiates a game of Avalon")
 async def avalon(ctx):
-    await ctx.send("Start a game of Avalon? [Y]es/[N]o")
+    await ctx.send("Initiate a game of Avalon? [Y]es/[N]o")
     def check(message):
         return message.author == ctx.author
     reply = await bot.wait_for("message", check=check)
     reply_content = reply.content.lower()
     if reply_content == "y" or reply_content == "yes":
-        await ctx.send("The quest begins!")
-    elif reply_content == "n" or reply_content == "no":
-        await ctx.send("Another day then.")
+        await ctx.send("The game initiates! Type `!join` to join the game.\nPlayer slots: 1 / 10")
+        bot.add_cog(cogs.Avalon(bot, ctx, ctx.author))
     else:
-        await ctx.send("You speak in riddles.")
+        await ctx.send("Another day then.")
 
 @bot.event
 async def on_command_error(ctx, error):
